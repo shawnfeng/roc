18238dd56cacbf2445a61311c291145a357c33e3
Shawn Feng <fengguangxiang@gmail.com>
2015-05-20 11:40:55 +0800

    add head

--- util/service/ServBase_test.go
+++ util/service/ServBase_test.go
@@ -1,3 +1,8 @@
+// Copyright 2014 The roc Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
 package rocserv
 
 import (
