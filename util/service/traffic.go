170accf57422a3c71dbaf137f416510f837896c4
zhenghe3119 <zhenghe3119@ipalfish.com>
2019-10-26 16:03:19 +0800

    feature: trace traffic patch 2

--- util/service/traffic.go
+++ util/service/traffic.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"github.com/opentracing/opentracing-go"
 	"github.com/shawnfeng/sutil/scontext"
-	"github.com/shawnfeng/sutil/slog"
+	"github.com/shawnfeng/sutil/slog/slog"
 	"github.com/uber/jaeger-client-go"
 	"net/http"
+	"strings"
 )
 
 const TrafficLogID = "TRAFFIC"
@@ -24,13 +25,15 @@ func httpTrafficLogMiddleware(next http.Handler) http.Handler {
 func trafficKVFromContext(ctx context.Context) (kv map[string]interface{}) {
 	kv = map[string]interface{}{}
 
+	kv["uid"], _ = scontext.GetUid(ctx)
+	kv["group"] = scontext.GetGroup(ctx)
+
 	span := opentracing.SpanFromContext(ctx)
 	if span == nil {
 		return
 	}
 
 	if spanCtx, ok := span.Context().(jaeger.SpanContext); ok {
-		kv["group"] = scontext.GetGroup(ctx)
 		kv["tid"] = fmt.Sprint(spanCtx.TraceID())
 		kv["sid"] = fmt.Sprint(spanCtx.SpanID())
 		kv["pid"] = fmt.Sprint(spanCtx.ParentID())
@@ -43,7 +46,13 @@ func logTrafficForHttpServer(ctx context.Context) {
 	for k, v := range trafficKVFromContext(ctx) {
 		kv[k] = v
 	}
-	logTrafficByKV(kv)
+	logTrafficByKV(ctx, kv)
+}
+
+func serviceFromServPath(spath string) string {
+	// NOTE: 若 sep 不为空, strings.Split 返回的字符串数组长度至少为 1
+	parts := strings.Split(spath, "/")
+	return parts[len(parts)-1]
 }
 
 func logTrafficForClientThrift(ctx context.Context, ct *ClientThrift, si *ServInfo) {
@@ -53,8 +62,8 @@ func logTrafficForClientThrift(ctx context.Context, ct *ClientThrift, si *ServIn
 	}
 
 	kv["srvid"] = si.Servid
-	kv["spath"] = ct.clientLookup.ServPath()
-	logTrafficByKV(kv)
+	kv["sname"] = serviceFromServPath(ct.clientLookup.ServPath())
+	logTrafficByKV(ctx, kv)
 }
 
 func logTrafficForClientGrpc(ctx context.Context, cg *ClientGrpc, si *ServInfo) {
@@ -64,11 +73,11 @@ func logTrafficForClientGrpc(ctx context.Context, cg *ClientGrpc, si *ServInfo) 
 	}
 
 	kv["srvid"] = si.Servid
-	kv["spath"] = cg.clientLookup.ServPath()
-	logTrafficByKV(kv)
+	kv["sname"] = serviceFromServPath(cg.clientLookup.ServPath())
+	logTrafficByKV(ctx, kv)
 }
 
-func logTrafficByKV(kv map[string]interface{}) {
+func logTrafficByKV(ctx context.Context, kv map[string]interface{}) {
 	bs, _ := json.Marshal(kv)
-	slog.Infof("%s %s", TrafficLogID, string(bs))
+	slog.Infof(ctx, "%s %s", TrafficLogID, string(bs))
 }
