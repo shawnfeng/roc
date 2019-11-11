b981fc76a1801167d34e211b4a3f913eb12e52a5
cx3ptr <bigpyer@126.com>
2019-11-11 16:07:50 +0800

    register tags to _metricRequestDuration before with

--- util/service/metrics.go
+++ util/service/metrics.go
@@ -13,9 +13,10 @@ var (
 	buckets = []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10}
 	// 目前只用作sla统计，后续通过修改标签作为所有微服务的耗时统计
 	_metricRequestDuration = xprom.NewHistogram(&xprom.HistogramVecOpts{
-		Namespace: namespacePalfish,
-		Name:      serverDurationSecond,
-		Help:      "sla calc request duration in seconds.",
-		Buckets:   buckets,
+		Namespace:  namespacePalfish,
+		Name:       serverDurationSecond,
+		Help:       "sla calc request duration in seconds.",
+		Buckets:    buckets,
+		LabelNames: []string{xprom.LabelServiceName, xprom.LabelServiceID, xprom.LabelInstance, xprom.LabelAPI, xprom.LabelSource, xprom.LabelType},
 	})
 )
