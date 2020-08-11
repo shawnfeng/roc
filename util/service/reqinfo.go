a6c454b1b8210c71ed7ec2b8e9972cb14c0186f0
eastfisher <eastfisher72@gmail.com>
2020-08-11 12:06:38 +0800

    refactor: inline func for better log

--- util/service/reqinfo.go
+++ util/service/reqinfo.go
@@ -3,7 +3,6 @@ package rocserv
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 
 	"gitlab.pri.ibanyu.com/middleware/seaweed/xlog"
 	"gitlab.pri.ibanyu.com/middleware/util/idl/gen-go/util/thriftutil"
@@ -21,9 +20,15 @@ func reqInfoInjectServerInterceptor() grpc.UnaryServerInterceptor {
 			return handler(ctx, req)
 		}
 
-		head, err := extractHeadFromMD(md)
-		if err != nil {
-			xlog.Warnf(ctx, "extractHeadFromMD error: %v", err)
+		values := md.Get(ContextKeyHead)
+		if len(values) == 0 {
+			xlog.Debugf(ctx, "no head in metadata")
+			return handler(ctx, req)
+		}
+
+		var head thriftutil.Head
+		if err := json.Unmarshal([]byte(values[0]), &head); err != nil {
+			xlog.Warnf(ctx, "unmarshal head error, string: %s, err: %v", values[0], err)
 			return handler(ctx, req)
 		}
 
@@ -43,7 +48,7 @@ func reqInfoInjectClientInterceptor() grpc.UnaryClientInterceptor {
 	) error {
 		head := ctx.Value(ContextKeyHead)
 		if head == nil {
-			xlog.Warnf(ctx, "context Head is nil")
+			xlog.Debugf(ctx, "context Head is nil")
 			return invoker(ctx, method, req, resp, cc, opts...)
 		}
 
@@ -55,41 +60,11 @@ func reqInfoInjectClientInterceptor() grpc.UnaryClientInterceptor {
 
 		bytes, err := json.Marshal(ohead)
 		if err != nil {
-			xlog.Warnf(ctx,"marshal head error, head: %+v, err: %v", head, err)
+			xlog.Warnf(ctx, "marshal head error, head: %+v, err: %v", head, err)
 			return invoker(ctx, method, req, resp, cc, opts...)
 		}
 
 		ctx = metadata.AppendToOutgoingContext(ctx, ContextKeyHead, string(bytes))
 		return invoker(ctx, method, req, resp, cc, opts...)
 	}
-}
-
-func extractHeadFromMD(md metadata.MD) (*thriftutil.Head, error) {
-	if md == nil {
-		return nil, fmt.Errorf("nil metadata")
-	}
-	values := md.Get(ContextKeyHead)
-	if len(values) == 0 {
-		return nil, fmt.Errorf("no head in metadata")
-	}
-	var head thriftutil.Head
-	if err := json.Unmarshal([]byte(values[0]), &head); err != nil {
-		return nil, fmt.Errorf("unmarshal head error, string: %s, err: %v", values[0], err)
-	}
-	return &head, nil
-}
-
-func addHeadIntoMD(md metadata.MD, head *thriftutil.Head) error {
-	if md == nil {
-		return fmt.Errorf("md is nil")
-	}
-	if head == nil {
-		return fmt.Errorf("head is nil")
-	}
-	bytes, err := json.Marshal(head)
-	if err != nil {
-		return fmt.Errorf("marshal head error, head: %+v, err: %v", head, err)
-	}
-	md.Append(ContextKeyHead, string(bytes))
-	return nil
 }
